126b76b0dc73b6ba48497aa1b3207df2e9575b27
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-11-19 14:21:02 +0700

    Extract bin tools and separate browser and server module

--- build.js
+++ build.js
@@ -1,19 +1,40 @@
-var fs = require('fs');
-var Compile = require('./compile.js');
 var DOMParser = require('xmldom').DOMParser;
-var files = ['test.html'];
+var Compile = require('./compile.js');
+var fs = require('fs');
+
 var Parser = new DOMParser();
 
-var pool = {};
+module.exports = function(files, as_module) {
+  if (!(files instanceof Array)) {
+    files = [files];
+  }
 
-files.forEach(function (file) {
-  template = fs.readFileSync(file, {encoding: 'utf8'});
-  new Compile(Parser.parseFromString(template), pool).build(file.split('.')[0]);
-});
+  var pool = {};
+  files.forEach(function(file) {
+    new Compile(
+      Parser.parseFromString(
+        fs.readFileSync(file, {encoding: 'utf8'})
+      ), pool
+    ).build(file.split('.')[0]);
+  });
 
-var pool_code = [];
-Object.keys(pool).forEach(function(template) {
-  pool_code.push('"' + template + '":' + pool[template]);
-});
+  var pool_code = [];
+  Object.keys(pool).forEach(function(template) {
+    pool_code.push('"' + template + '":' + pool[template]);
+  });
 
-console.log('{' + pool_code.join(',') + '}');
+  var lines = [];
+  if (as_module) {
+    lines = [
+      'var jext = require("jext");',
+      'module.exports = {' + pool_code.join(',') + '}'
+    ];
+  } else {
+    lines = [
+      'var template_list = {' + pool_code.join(',') + '};',
+      'window.templates = jext.pool(template_list);'
+    ];
+  }
+
+  return lines.join("\n");
+};
